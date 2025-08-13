df57604862e64a2d99f7da924082d5e69985755d
Alexey <erg163@ya.ru>
2025-08-13 10:18:47 +0400

    Initial commit

---
+++ src/test/java/ru/netology/rest/DebitCardOrderTest.java
@@ -0,0 +1,59 @@
+package ru.netology.rest;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DebitCardOrderTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup(); //авто настройка драйверов для хрома
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+//  options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+
+
+    }
+
+
+    @Test
+    void NestDebitCardApplication() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Клоков Алексей");
+        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79033003333");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+                assertTrue(result.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
+
+
+    }
+}
